f84bafd9615b27a4e7fca619539b50dd250fc88c
Mosaddik Billah <mosaddik.billah08@gmail.com>
2024-08-02 11:36:30 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,103 @@
+var nameError = document.getElementById('name-error');
+var phoneError = document.getElementById('phone-error');
+var emailError = document.getElementById('email-error');
+var massegeError = document.getElementById('massege-error');
+var submitError = document.getElementById('submit-error');
+var messageContainer = document.getElementById('messageContainer');
+
+function validateName() {
+    var name = document.getElementById('contact-name').value;
+    if (name.length == 0) {
+        nameError.innerHTML = 'Name is required';
+        return false;
+    }
+    if (!name.match(/^[A-Za-z]*\s{1}[A-Za-z]*$/)) {
+        nameError.innerHTML = 'Write full name';
+        return false;
+    }
+    nameError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validatePhone() {
+    var phone = document.getElementById('contact-phone').value;
+
+    if (phone.length == 0) {
+        phoneError.innerHTML = 'Phone No is required';
+        return false;
+    }
+    if (phone.length !== 11) {
+        phoneError.innerHTML = 'Phone No should be 11 digits';
+        return false;
+    }
+    if (!phone.match(/^[0-9]{11}$/)) {
+        phoneError.innerHTML = 'Only digits please';
+        return false;
+    }
+    phoneError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validateEmail() {
+    var email = document.getElementById('contact-email').value;
+    if (email.length == 0) {
+        emailError.innerHTML = 'Email is required';
+        return false;
+    }
+    if (!email.match(/^[A-Za-z\._\-[0-9]*[@][A-Za-z]*[\.][a-z]{2,4}$/)) {
+        emailError.innerHTML = 'Email invalid';
+        return false;
+    }
+    emailError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validateMessage() {
+    var massege = document.getElementById('contact-massege').value;
+    var required = 30;
+    var left = required - massege.length;
+    if (left > 0) {
+        massegeError.innerHTML = left + ' more characters required';
+        return false;
+    }
+    massegeError.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+    return true;
+}
+
+function validateForm() {
+    if (!validateName() || !validatePhone() || !validateEmail() || !validateMessage()) {
+        submitError.style.display = 'block';
+        submitError.innerHTML = 'Please fix error to submit';
+        setTimeout(function () { submitError.style.display = 'none'; }, 3000);
+        return false;
+    }
+    submitForm();
+    return false;
+}
+
+function submitForm() {
+    var form = document.getElementById('contactForm');
+    var formData = new FormData(form);
+
+    fetch(form.action, {
+        method: 'POST',
+        body: formData
+    }).then(response => response.json())
+        .then(data => {
+            if (data.result === "success") {
+                var customMessage = `Form submitted successfully! Your row number is ${data.row}.`;
+                messageContainer.innerHTML = customMessage;
+            } else {
+                var errorMessage = "Form submission failed. Please try again.";
+                messageContainer.innerHTML = errorMessage;
+            }
+        })
+        .catch(error => {
+            messageContainer.innerHTML = "Please fix error and  Try again";
+        });
+}
+
+document.getElementById('contactForm').addEventListener('submit', function (event) {
+    event.preventDefault();
+    validateForm();
+});
